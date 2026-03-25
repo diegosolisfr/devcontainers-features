793e81eb6527691812e7e25eca4a9dc50b85d975
Diego Solis Franco <dfranco@babbel.com>
2026-03-25 12:51:51 +0100

    chore: update tests

---
+++ test/_global/hello_global.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# The 'test/_global' folder is a special test folder that is not tied to a single feature.
+#
+# This test file is executed against a running container constructed
+# from the value of 'hello_global' in the tests/_global/scenarios.json file.
+#
+# This test can be run with the following command (from the root of this repo)
+#    devcontainer features test --global-scenarios-only .
+
+set -e
+
+# Optional: Import test library bundled with the devcontainer CLI
+source dev-container-features-test-lib
+
+echo -e "The result of the 'hello' command will be:\n"
+hello
+echo -e "\n"
+
+# Feature-specific tests
+# The 'check' command comes from the dev-container-features-test-lib.
+check "check greeting with 'Greetings'" bash -c "hello | grep 'Greetings, $(whoami)'"
+
+# Report result
+# If any of the checks above exited with a non-zero exit code, the test will fail.
+reportResults
